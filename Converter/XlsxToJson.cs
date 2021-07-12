c085c7dca1a70454d4dcdccc1c2d69b682331222
Jerson Brito <jerson.silva@ambevtech.com.br>
2021-07-12 12:59:47 -0300

    get types

--- Converter/XlsxToJson.cs
+++ Converter/XlsxToJson.cs
@@ -1,5 +1,6 @@
 ﻿using ClosedXML.Excel;
 using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.IO;
@@ -23,22 +24,24 @@ namespace Converter
         }
 
         public static implicit operator XlsxToJson(Stream stream)
-            => new XlsxToJson(stream);
+            => new(stream);
 
         public object Json
             => JsonConvert.SerializeObject(new { table = _dataTable }, Formatting.Indented);
 
         private void SetBody()
         {
-            var body = _worksheet.Rows(2, _worksheet.Rows().Count());
+            var body = _worksheet.Rows(2, _worksheet.Rows().Count()-1);
             foreach (var row in body)
             {
                 int i = 0;
                 var dataRow = _dataTable.NewRow();
-                foreach (var cell in row.Cells())
+
+                foreach (var cell in row.Cells(false).Take(_headers.Count()))
                 {
-                    dataRow[_headers.ElementAt(i++)] = cell.Value.ToString();
+                    dataRow[_headers.ElementAt(i++)] = cell.Value;
                 }
+
                 _dataTable.Rows.Add(dataRow);
             }
         }
@@ -46,9 +49,27 @@ namespace Converter
         private void SetHeader()
         {
             _headers = _worksheet.FirstRowUsed().Cells().Select(c => c.Value.ToString());
-            foreach (var cell in _headers)
+
+            var types = _worksheet.Rows()
+                                  .Skip(1)
+                                  .FirstOrDefault(x => x.CellsUsed().Count() == _headers.Count())
+                                  .Cells()
+                                  .Select(x => x.DataType);
+
+            var i = 0;
+            foreach (var name in _headers)
             {
-                _dataTable.Columns.Add(cell);
+                var type = types.ElementAt(i++) switch
+                {
+                    XLDataType.Text => typeof(string),
+                    XLDataType.Number => typeof(decimal),
+                    XLDataType.Boolean => typeof(bool),
+                    XLDataType.DateTime => typeof(DateTime),
+                    XLDataType.TimeSpan => typeof(DateTime),
+                    _ => typeof(string)
+                };
+
+                _dataTable.Columns.Add(name, type);
             }
         }
     }
